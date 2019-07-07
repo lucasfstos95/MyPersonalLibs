3246f3d5fcdeafd0eea132405e7e99105c46a221
lucasfstos95 <lucas.fstos95@gmail.com>
2019-07-07 13:06:31 -0300

    Atualização do construtor da classe.

--- RS485_Module/RS485-simple/lib/RS485Module/RS485Module.cpp
+++ RS485_Module/RS485-simple/lib/RS485Module/RS485Module.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include "RS485Module.h"
 
-   
+
 #define serial Serial
 //========== DIRECTIVES ===============//
 //#define SIZE_OF_BUFFER 256
@@ -12,10 +12,15 @@ char buffer[SERIAL_RX_BUFFER_SIZE];                //buffer master to receive an
 
 //constructor
 RS485Module::RS485Module(int serial){
+
+    if(serial == 0){
+         #define serialPrint(text) Serial.print(text)
+         #define serialPrintln(text) Serial.println(text)
+         #define serialReadString(text) Serial.readStringUntil(text)
+         #define serialWrite(text) Serial.write(text)
+         #define serialBegin(baud_rate) Serial.begin(baud_rate)
+    }
     /*
-    #if(serial == 0)
-         #define serial Serial
-    #endif
     #if (serial == 1)
          #define serial Serial2
     #endif
@@ -24,15 +29,15 @@ RS485Module::RS485Module(int serial){
     #endif
     #if (serial == 3)
          #define serial Serial3
-    #endif 
-    */          
-            
+    #endif
+    */
+
 }
 
 void RS485Module::read(){
     int i = 0;
     //size_t buffer_size;
-    String msg = serial.readStringUntil('\n');
+    String msg = serialReadString('\n');
     int tam = msg.length();
     for(i=0; i<tam; i++){
         buffer[i] = (char)msg.charAt(i);
@@ -41,11 +46,11 @@ void RS485Module::read(){
 
 void RS485Module::print(const char *msg){
     for(int i=0; i<SERIAL_TX_BUFFER_SIZE; i++){
-        serial.write(buffer[i]);
+        serialWrite(buffer[i]);
     }
-    serial.write('\n');
+    serialWrite('\n');
 }
 
 void RS485Module::begin(int baud){
-    serial.begin(baud);
+    serialBegin(baud);
 }
