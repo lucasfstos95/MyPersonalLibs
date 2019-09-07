459244237973c5833962e3b0118a5a537164dd6b
lucasfstos95 <lucasfstos95@gmail.com>
2019-09-07 09:59:04 -0300

    Adding softwareSerial feature(not working yet)

--- RS485_Module/RS485-simple/lib/RS485Module/RS485Module.h
+++ RS485_Module/RS485-simple/lib/RS485Module/RS485Module.h
@@ -4,14 +4,25 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
+//#define SOFTWARE_SERIAL_ENABLED
 
     class RS485Module
     {
         public:
-            HardwareSerial *_MySerial;
-            RS485Module(HardwareSerial &serial);
+
+            #ifndef SOFTWARE_SERIAL_ENABLED
+                HardwareSerial *_MySerial;
+                RS485Module(HardwareSerial &serial);
+            #else
+                SoftwareSerial *_MySerial;
+                RS485Module(SoftwareSerial &serial);
+            #endif
+            
+            
+            
             void read();
             void print(const char *msg);
+            void println(const char *msg);
             void begin(int baud);
             
         
